36d645f93070858c6e16bbaab8ba64edea410b54
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-10-06 19:56:16 +0200

    Removed unparse method

--- com.kaleidoscope.core.framework/src/com/kaleidoscope/core/framework/workflow/adapters/DeltaAdapter.java
+++ com.kaleidoscope.core.framework/src/com/kaleidoscope/core/framework/workflow/adapters/DeltaAdapter.java
@@ -3,8 +3,5 @@ package com.kaleidoscope.core.framework.workflow.adapters;
 import com.kaleidoscope.core.delta.javabased.Delta;
 
 public interface DeltaAdapter<ModelDelta extends Delta, ArtefactDelta, Model> {
-	
 	public ModelDelta parse(ArtefactDelta ad, Model m);
-	public ArtefactDelta unparse(ModelDelta d, Model a);
-
 }

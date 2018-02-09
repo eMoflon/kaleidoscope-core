60ad4d4f156ca2348cf73bf5169df3c8a62f37eb
Srijani Ghosh <srijanighosh87@gmail.com>
2018-02-09 22:50:06 +0100

    committed on 09.02.2018

---
+++ com.kaleidoscope.core.aux.simpleexcel/src/com/kaleidoscope/core/auxiliary/simpleexcel/artefactadapter/ExcelDelta.java
@@ -0,0 +1,261 @@
+/**
+ * 
+ */
+package com.kaleidoscope.core.auxiliary.simpleexcel.artefactadapter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.kaleidoscope.core.auxiliary.simpleexcel.utils.UnableToEditExcelFile;
+
+/**
+ * @author Srijani
+ *
+ */
+public class ExcelDelta {
+
+	private Path filePath;
+	private XSSFWorkbook workbook;
+	private String file;
+
+	private List<List<Object>> excelOperations;
+	private List<List<Object>> excelOperationsAddNodes;
+	private List<List<Object>> excelOperationsDeleteNodes;
+	private List<List<Object>> excelOperationsAddEdges;
+	private List<List<Object>> excelOperationsDeleteEdges;
+
+	/**
+	 * @param path
+	 * @param operationList2
+	 */
+	public ExcelDelta(Path path, List<List<Object>> excelOperations) {
+		this.filePath = path;
+		this.excelOperations = excelOperations;
+		splitOperations();
+	}
+
+	/**
+	 * Split functions based on the operation types
+	 */
+	private void splitOperations() {
+		for (List<Object> list : excelOperations) {
+			String opName = list.get(0).toString();
+			switch (opName) {
+			case "ADD_NODE":
+				excelOperationsAddNodes.add(list);
+				break;
+
+			case "ADD_EDGE":
+				excelOperationsAddEdges.add(list);
+				break;
+
+			case "DELET_NODE":
+				excelOperationsDeleteNodes.add(list);
+				break;
+
+			case "DELETE_EDGE":
+				excelOperationsDeleteEdges.add(list);
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	public void execute() throws UnableToEditExcelFile {
+		System.out.println("Printing excel operations");
+		for (List<Object> list : excelOperations) {
+			String operationName = list.get(0).toString();
+			System.out.println(operationName);
+			switch (operationName) {
+			case "ADD_FILE":
+				fileOperation("ADD_FILE", list.get(1));
+				break;
+
+			case "ADD_SHEET":
+				sheetOperation("ADD_SHEET", list.get(1));
+				break;
+
+			case "DELETE_SHEET":
+				sheetOperation("DELETE_SHEET", list.get(1));
+				break;
+
+			case "ADD_ROW":
+				rowOperation("ADD_ROW", list.get(1));
+				break;
+
+			case "ADD_CELL":
+
+				break;
+
+			default:
+				System.out.println("OPERATION NOT FOUND IN EXCEL API");
+				break;
+			}
+		}
+
+	}
+
+	/**
+	 * ROW OPERATIONS, ADD_ROW & DELETE_ROW
+	 * 
+	 * @param string
+	 * @param object
+	 */
+	private void rowOperation(String string, Object object) {
+		// at this moment, there is nothing in the object.
+
+		switch (string) {
+		case "ADD_ROW":
+
+			break;
+
+		case "DELETE_ROW":
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * SHEET OPERATIONS, ADD_SHEET & DELETE_SHEET
+	 * 
+	 * @param string
+	 * @param object
+	 * @throws UnableToEditExcelFile
+	 */
+	private void sheetOperation(String string, Object object) throws UnableToEditExcelFile {
+		HashMap<String, Object> sheetDataMap = (HashMap<String, Object>) object;
+		String sheetName = "";
+		int sheedId = 0;
+		if (null != sheetDataMap) {
+			if (sheetDataMap.containsKey("SHEET_NAME"))
+				if (sheetDataMap.get("SHEET_NAME") instanceof String)
+					sheetName = (String) sheetDataMap.get("SHEET_NAME");
+				else
+					throw new UnableToEditExcelFile("SHEET NAME NOT READBLE...");
+			if (sheetDataMap.containsKey("SHEET_ID"))
+				if (sheetDataMap.get("SHEET_ID") instanceof Integer)
+					sheedId = (int) sheetDataMap.get("SHEET_ID");
+				else
+					throw new UnableToEditExcelFile("SHEET ID NOT READBLE...");
+		}
+
+		switch (string) {
+		case "ADD_SHEET":
+			createSheet(sheetName, sheedId);
+			break;
+
+		case "DELETE_SHEET":
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * Creates sheet in a excel file
+	 * 
+	 * @param sheetName
+	 * @param sheedId
+	 * @throws UnableToEditExcelFile
+	 */
+	private void createSheet(String sheetName, int sheedId) throws UnableToEditExcelFile {
+		try {
+
+			File file = new File(this.file);
+			if (file.exists()) {
+				final InputStream is = new FileInputStream(file);
+				workbook = new XSSFWorkbook(is);
+				workbook.createSheet(sheetName);
+
+				FileOutputStream fileOutputStream = new FileOutputStream(file);
+				workbook.write(fileOutputStream);
+				fileOutputStream.close();
+			} else {
+				throw new UnableToEditExcelFile("FILE NOT FOUND..");
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * ADD_FILE & DELETE_FILE
+	 * 
+	 * @param string
+	 * @param object
+	 * @throws UnableToEditExcelFile
+	 */
+	private void fileOperation(String string, Object object) throws UnableToEditExcelFile {
+		HashMap<String, Object> fileDataMap = (HashMap<String, Object>) object;
+		String fileName = "";
+		String filePath = "";
+		if (null != fileDataMap) {
+			if (fileDataMap.containsKey("FILE_NAME")) {
+				if (fileDataMap.get("FILE_NAME") instanceof String)
+					fileName = (String) fileDataMap.get("FILE_NAME");
+				else
+					throw new UnableToEditExcelFile("FILE NAME NOT READBLE...");
+			}
+			if (fileDataMap.containsKey("FILE_PATH")) {
+				if (fileDataMap.get("FILE_PATH") instanceof String)
+					filePath = (String) fileDataMap.get("FILE_PATH");
+				else
+					throw new UnableToEditExcelFile("FILE PATH NOT READBLE...");
+			}
+		}
+		switch (string) {
+		case "ADD_FILE":
+			createFile(fileName, filePath);
+			break;
+		case "DELETE_FILE":
+			// TODO
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * Creates a new file
+	 * 
+	 * @param fileName
+	 * @param filePath
+	 * @throws UnableToEditExcelFile
+	 */
+	private void createFile(String fileName, String filePath) throws UnableToEditExcelFile {
+		File file = new File(filePath + "\\" + fileName);
+		if (file.exists()) {
+			throw new UnableToEditExcelFile("File already exists!");
+		} else {
+			this.file = filePath + "\\" + fileName;
+			System.out.println("Creating new file...");
+			XSSFWorkbook xssfWorkbook = new XSSFWorkbook();
+
+			try {
+				FileOutputStream fileOutputStream = new FileOutputStream(file);
+				xssfWorkbook.write(fileOutputStream);
+				fileOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+}
---
+++ com.kaleidoscope.core.aux.simpleexcel/src/com/kaleidoscope/core/auxiliary/simpleexcel/artefactadapter/ExcelDeltaAdapter.java
@@ -0,0 +1,310 @@
+/**
+ * 
+ */
+package com.kaleidoscope.core.auxiliary.simpleexcel.artefactadapter;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+
+import com.kaleidoscope.core.delta.javabased.operational.AddEdgeOp;
+import com.kaleidoscope.core.delta.javabased.operational.AddNodeOp;
+import com.kaleidoscope.core.delta.javabased.operational.DeleteEdgeOp;
+import com.kaleidoscope.core.delta.javabased.operational.DeleteNodeOp;
+import com.kaleidoscope.core.delta.javabased.operational.Operation;
+import com.kaleidoscope.core.delta.javabased.operational.OperationalDelta;
+import com.kaleidoscope.core.framework.workflow.adapters.DeltaOutputAdapter;
+
+import Delta.AddNodeOP;
+import Delta.Edge;
+import Delta.impl.AddNodeOPImpl;
+import Simpleexcel.Cell;
+import Simpleexcel.File;
+import Simpleexcel.RowObject;
+import Simpleexcel.Sheet;
+
+/**
+ * @author Srijani
+ *
+ */
+public class ExcelDeltaAdapter implements DeltaOutputAdapter<OperationalDelta, ExcelDelta, Path> {
+
+	List<Operation> operations = new ArrayList<Operation>();
+	List<Operation> sortedOperationsEMF = new ArrayList<Operation>();
+	List<Operation> sortedOperationsExcelPOI = new ArrayList<Operation>();
+	List<List<Object>> excelOperations = new ArrayList<List<Object>>();
+
+	private List<Operation> fileNodeAddDeleteOperations = new ArrayList<Operation>();
+	private List<Operation> sheetNodeAddDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> fileToSheetEdgeAddAndDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> cellNodeAddDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> sheetToCellEdgeAddAndDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> rowNodeAddDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> sheetToRowEdgeAddAndDeleteOperations = new ArrayList<Operation>();
+
+	private List<Operation> rowToCellEdgeAddAndDeleteOperations = new ArrayList<Operation>();
+
+	@Override
+	public ExcelDelta unparse(OperationalDelta od, Path path) {
+
+		// get the operations in list
+		operations = od.getOperations();
+
+		// sort the operations - EMF
+		sortOperations();
+
+		// sort again for EXCEL API
+		translateSortedOperationsForExcelApi();
+
+		// assign excel tasks in order
+		convertToExcelAssignments();
+
+		// connect with ExcelDelta
+		ExcelDelta excelDelta = new ExcelDelta(path, excelOperations);
+		return excelDelta;
+	}
+
+	/**
+	 * This method takes sortedOperationsExcelPOI list and convert it to
+	 * corresponding excel tasks
+	 */
+	private void convertToExcelAssignments() {
+		for (Operation operation : sortedOperationsExcelPOI) {
+			List<Object> innerOperations = new ArrayList<Object>();
+
+			// ================ ADD NODE ===================
+			if ((operation instanceof AddNodeOp)) {
+				// ============== ADD FILE =================
+				if (((AddNodeOp) operation).getNode() instanceof Simpleexcel.File) {
+					innerOperations.add("ADD_FILE");
+					HashMap<String, String> innerMap = new HashMap<String, String>();
+					innerMap.put("FILE_NAME", ((Simpleexcel.File) ((AddNodeOp) operation).getNode()).getFileName());
+					innerMap.put("FILE_PATH", ((Simpleexcel.File) ((AddNodeOp) operation).getNode()).getPath());
+
+					innerOperations.add(innerMap);
+				}
+				// ============== ADD SHEET =================
+				if (((AddNodeOp) operation).getNode() instanceof Simpleexcel.Sheet) {
+					innerOperations.add("ADD_SHEET");
+					HashMap<String, Object> innerMap = new HashMap<String, Object>();
+					innerMap.put("SHEET_NAME", ((Simpleexcel.Sheet) ((AddNodeOp) operation).getNode()).getSheetName());
+					innerMap.put("SHEET_ID", ((Simpleexcel.Sheet) ((AddNodeOp) operation).getNode()).getSheetId());
+
+					innerOperations.add(innerMap);
+				}
+				
+				// ============== DELETE SHEET =================
+				if (((DeleteNodeOp) operation).getNode() instanceof Simpleexcel.Sheet) {
+					innerOperations.add("DELETE_SHEET");
+					HashMap<String, Object> innerMap = new HashMap<String, Object>();
+					innerMap.put("SHEET_NAME", ((Simpleexcel.Sheet) ((AddNodeOp) operation).getNode()).getSheetName());
+				}
+				
+				// ============== ADD ROW =================
+				if (((AddNodeOp) operation).getNode() instanceof Simpleexcel.RowObject) {
+					innerOperations.add("ADD_ROW");
+				}
+				// ============== ADD CELL =================
+				if (((AddNodeOp) operation).getNode() instanceof Simpleexcel.Cell) {
+					innerOperations.add("ADD_CELL");
+					HashMap<String, Object> innerMap = new HashMap<String, Object>();
+					innerMap.put("CELL_TEXT", ((Simpleexcel.Cell) ((AddNodeOp) operation).getNode()).getText());
+					innerMap.put("CELL_ID", ((Simpleexcel.Cell) ((AddNodeOp) operation).getNode()).getCellId());
+					innerMap.put("CELL_COMMENTS",
+							((Simpleexcel.Cell) ((AddNodeOp) operation).getNode()).getCellComments());
+					innerMap.put("CELL_COLOR",
+							((Simpleexcel.Cell) ((AddNodeOp) operation).getNode()).getBackgroundColor());
+
+					innerOperations.add(innerMap);
+				}
+			}
+			
+			if(null!=innerOperations && !innerOperations.isEmpty())
+				excelOperations.add(innerOperations);
+		}
+	}
+
+	/**
+	 * Sorted for being usable by POI API
+	 */
+	private void translateSortedOperationsForExcelApi() {
+
+		// create file
+		sortedOperationsExcelPOI.addAll(fileNodeAddDeleteOperations);
+
+		// create sheets
+		sortedOperationsExcelPOI.addAll(sheetNodeAddDeleteOperations);
+		sortedOperationsExcelPOI.addAll(fileToSheetEdgeAddAndDeleteOperations);
+
+		// create rows
+		sortedOperationsExcelPOI.addAll(rowNodeAddDeleteOperations);
+		sortedOperationsExcelPOI.addAll(sheetToRowEdgeAddAndDeleteOperations);
+
+		// create cells
+		sortedOperationsExcelPOI.addAll(cellNodeAddDeleteOperations);
+		sortedOperationsExcelPOI.addAll(rowToCellEdgeAddAndDeleteOperations);
+		
+		printOperationsFromList(sortedOperationsExcelPOI, "Sorted operations for EXCEL");
+
+	}
+
+	/**
+	 * Sort the operations to make sense for EMF
+	 */
+	private void sortOperations() {
+		//printOperationsFromList(operations, "Original List");
+
+		for (Operation operation : operations) {
+			// ==============FILE=================
+			// add node - file
+			if ((operation instanceof AddNodeOp) && ((AddNodeOp) operation).getNode() instanceof Simpleexcel.File) {
+				fileNodeAddDeleteOperations.add(operation);
+			}
+			// delete node - file
+			if ((operation instanceof DeleteNodeOp)
+					&& ((DeleteNodeOp) operation).getNode() instanceof Simpleexcel.File) {
+				fileNodeAddDeleteOperations.add(operation);
+			}
+
+			// =============SHEET==================
+			// add node - sheet
+			if ((operation instanceof AddNodeOp) && ((AddNodeOp) operation).getNode() instanceof Simpleexcel.Sheet) {
+				sheetNodeAddDeleteOperations.add(operation);
+			}
+			// delete node - sheet
+			if ((operation instanceof DeleteNodeOp)
+					&& ((DeleteNodeOp) operation).getNode() instanceof Simpleexcel.Sheet) {
+				sheetNodeAddDeleteOperations.add(operation);
+			}
+
+			// ==========FILE->SHEET EDGE=====================
+			// add edge file--> sheet
+			if ((operation instanceof AddEdgeOp) && ((AddEdgeOp) operation).getEdge().getSrc() instanceof File
+					&& ((AddEdgeOp) operation).getEdge().getTrg() instanceof Sheet) {
+				fileToSheetEdgeAddAndDeleteOperations.add(operation);
+			}
+			// Delete edge
+			if ((operation instanceof DeleteEdgeOp) && ((DeleteEdgeOp) operation).getEdge().getSrc() instanceof File
+					&& ((DeleteEdgeOp) operation).getEdge().getTrg() instanceof Sheet) {
+				fileToSheetEdgeAddAndDeleteOperations.add(operation);
+			}
+
+			// =============CELL==================
+			// add node - cell
+			if ((operation instanceof AddNodeOp) && ((AddNodeOp) operation).getNode() instanceof Simpleexcel.Cell) {
+				cellNodeAddDeleteOperations.add(operation);
+			}
+			// delete node - cell
+			if ((operation instanceof DeleteNodeOp)
+					&& ((DeleteNodeOp) operation).getNode() instanceof Simpleexcel.Cell) {
+				cellNodeAddDeleteOperations.add(operation);
+			}
+
+			// ==============SHEET->CELL EDGE=================
+			// add edge sheet--> cell
+			if ((operation instanceof AddEdgeOp) && ((AddEdgeOp) operation).getEdge().getSrc() instanceof Sheet
+					&& ((AddEdgeOp) operation).getEdge().getTrg() instanceof Cell) {
+				sheetToCellEdgeAddAndDeleteOperations.add(operation);
+			}
+			// Delete edge
+			if ((operation instanceof DeleteEdgeOp) && ((DeleteEdgeOp) operation).getEdge().getSrc() instanceof Sheet
+					&& ((DeleteEdgeOp) operation).getEdge().getTrg() instanceof Cell) {
+				sheetToCellEdgeAddAndDeleteOperations.add(operation);
+			}
+
+			// ==============ROW=================
+			// add node - row
+			if ((operation instanceof AddNodeOp)
+					&& ((AddNodeOp) operation).getNode() instanceof Simpleexcel.RowObject) {
+				rowNodeAddDeleteOperations.add(operation);
+			}
+			// delete node - row
+			if ((operation instanceof DeleteNodeOp)
+					&& ((DeleteNodeOp) operation).getNode() instanceof Simpleexcel.RowObject) {
+				rowNodeAddDeleteOperations.add(operation);
+			}
+
+			// ==============SHEET->ROW EDGE=================
+			// add edge sheet--> row
+			if ((operation instanceof AddEdgeOp) && ((AddEdgeOp) operation).getEdge().getSrc() instanceof Sheet
+					&& ((AddEdgeOp) operation).getEdge().getTrg() instanceof RowObject) {
+				sheetToRowEdgeAddAndDeleteOperations.add(operation);
+			}
+			// Delete edge
+			if ((operation instanceof DeleteEdgeOp) && ((DeleteEdgeOp) operation).getEdge().getSrc() instanceof Sheet
+					&& ((DeleteEdgeOp) operation).getEdge().getTrg() instanceof RowObject) {
+				sheetToRowEdgeAddAndDeleteOperations.add(operation);
+			}
+
+			// ==============ROW->CELL EDGE=================
+			// add edge row-->cell
+			if ((operation instanceof AddEdgeOp) && ((AddEdgeOp) operation).getEdge().getSrc() instanceof RowObject
+					&& ((AddEdgeOp) operation).getEdge().getTrg() instanceof Cell) {
+				rowToCellEdgeAddAndDeleteOperations.add(operation);
+			}
+			// Delete edge
+			if ((operation instanceof DeleteEdgeOp)
+					&& ((DeleteEdgeOp) operation).getEdge().getSrc() instanceof RowObject
+					&& ((DeleteEdgeOp) operation).getEdge().getTrg() instanceof Cell) {
+				rowToCellEdgeAddAndDeleteOperations.add(operation);
+			}
+		}
+
+		sortedOperationsEMF.addAll(fileNodeAddDeleteOperations);
+		sortedOperationsEMF.addAll(sheetNodeAddDeleteOperations);
+
+		sortedOperationsEMF.addAll(fileToSheetEdgeAddAndDeleteOperations);
+
+		sortedOperationsEMF.addAll(cellNodeAddDeleteOperations);
+		sortedOperationsEMF.addAll(sheetToCellEdgeAddAndDeleteOperations);
+
+		sortedOperationsEMF.addAll(rowNodeAddDeleteOperations);
+		sortedOperationsEMF.addAll(sheetToRowEdgeAddAndDeleteOperations);
+
+		sortedOperationsEMF.addAll(rowToCellEdgeAddAndDeleteOperations);
+
+		//printOperationsFromList(sortedOperationsEMF, "Sorted Operations for EMF");
+	}
+
+	/**
+	 * Test method to print all operations in details
+	 * 
+	 * @param operations2
+	 * @param string 
+	 */
+	private void printOperationsFromList(List<Operation> operations2, String string) {
+		System.out.println("Printing list : "+ string);
+
+		for (Operation operation : operations2) {
+
+			if (operation instanceof AddNodeOp) {
+				System.out.println("--> Add Node --> " + ((AddNodeOp) operation).getNode());
+			} else {
+				if (operation instanceof AddEdgeOp) {
+					System.out.println("--> Add Edge --> " + ((AddEdgeOp) operation).getEdge().getSrc() + " => "
+							+ ((AddEdgeOp) operation).getEdge().getTrg());
+				} else {
+					if (operation instanceof DeleteNodeOp) {
+						System.out.println("--> Delete Node --> " + ((DeleteNodeOp) operation).getNode());
+					} else {
+						if (operation instanceof DeleteEdgeOp) {
+							System.out.println("--> Delete Node --> " + ((DeleteEdgeOp) operation).getEdge().getSrc()
+									+ " => " + ((DeleteEdgeOp) operation).getEdge().getTrg());
+						}
+					}
+				}
+			}
+		}
+	}
+}
